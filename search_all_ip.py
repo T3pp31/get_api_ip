046e064a680cd5ae143780eaf6a8654707b359c8
Teppei.F <37261985+T3pp31@users.noreply.github.com>
2023-04-25 17:41:26 +0900

    add

--- search_all_ip.py
+++ search_all_ip.py
@@ -2,18 +2,34 @@ from src.get_API_ip import get_API_ip
 from src.get_API_url import get_API_url
 from tqdm import tqdm
 
-search_urls = ['https://www.mlb.com','https://google.com']
+f = open('search.txt','r')
+read_search_urls = f.readlines()
+search_urls = [search_url.replace('\n','') for search_url in read_search_urls]
+
+#search_urls = ['https://www.mlb.com','http://portal.kyodonews.jp/']
 results = {}
 
 for search_url in tqdm(search_urls):
+
     result_url = get_API_url(search_url)
+
     result = {}
-    for url in result_url:
+    for url in tqdm(result_url):
         ip = get_API_ip(url)
-        result[url] = ip
+        if ip != '0.0.0.0':
+            result[url] = ip
+
     results[search_url] = result
 
 
 with open('result.txt','w') as file:
-    file.write(results)
+    #get results' key
+    for url in results.keys():
+        #
+        for (key,value) in zip(results[url].keys(),results[url].values()):
+            print(f'{url}|{key}|{value}')
+            file.write(f'{url}|{key}|{value}\n')
+
+
+
 
